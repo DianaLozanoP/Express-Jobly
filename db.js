15ea0ad9d06f9b2666ec2be51551ff2478974962
dianaloz <dlbogota2104@gmail.com>
2024-04-18 12:01:28 -0400

    Added postgresql

--- db.js
+++ db.js
@@ -1,25 +1,47 @@
 "use strict";
 /** Database setup for jobly. */
+// const { Client } = require("pg");
+// const password = require('./password')
+
+// let database;
+
+// // If we're running in test "mode", use our test db
+// // Make sure to create both databases!
+// if (process.env.NODE_ENV === "test") {
+//   database = "jobly_test";
+// } else {
+//   database = "jobly";
+// }
+
+// let db = new Client({
+//   user: 'dianaloz',
+//   host: '/var/run/postgresql',
+//   database: database,
+//   password: password,
+//   port: 5432
+
+// });
+
+// db.connect()
+//   .then(() => {
+//     console.log('Connected to the database');
+//     // Perform database operations here
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to the database:', err);
+//     // Additional error details for debugging
+//     console.error('Error code:', err.code); // PostgreSQL error code
+//     console.error('Error message:', err.message); // Description of the error
+//     console.error('Error stack:', err.stack); // Stack trace
+//   });
+
+// module.exports = db;
+
 const { Client } = require("pg");
-const password = require('./password')
+const connectionString = "postgres://jbvosced:eonPrmxq8mywjFMCR-yLlN0ZbIIVtW6v@kala.db.elephantsql.com/jbvosced";
 
-let database;
-
-// If we're running in test "mode", use our test db
-// Make sure to create both databases!
-if (process.env.NODE_ENV === "test") {
-  database = "jobly_test";
-} else {
-  database = "jobly";
-}
-
-let db = new Client({
-  user: 'dianaloz',
-  host: '/var/run/postgresql',
-  database: database,
-  password: password,
-  port: 5432
-
+const db = new Client({
+  connectionString: connectionString
 });
 
 db.connect()
